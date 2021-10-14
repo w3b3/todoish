1bf38bbaf2941b9812f225b2d34f5ccca2e11e1b
Daniel Santos <iam@w-b.dev>
2021-10-14 09:04:32 -0300

    add drawer and move lang settings into it

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Clock from "./clock/clock";
 import { TaskManagement } from "./taskManagement/TaskManagement";
-import { AppSettingsProvider } from "../context/appSettingsContext";
+import AppSettingsContext from "../context/appSettingsContext";
 import { Header } from "./Header";
 import { Logo } from "./Logo";
-import { Button } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  SwipeableDrawer,
+  Typography,
+} from "@material-ui/core";
+import { Locale } from "../types/types";
+import { STRINGS } from "../strings/strings";
 
 function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { locale, setLocale } = useContext(AppSettingsContext);
+  const handleLocaleClick = () => {
+    setLocale(locale === "pt-br" ? "en-us" : "pt-br");
+  };
   return (
-    <AppSettingsProvider>
+    <>
       <Header>
         <Clock />
         <Logo />
-        {/*<CountdownModal />*/}
         <Button
           variant={"text"}
           title="Menu"
           startIcon={<i className="fas fa-bars" />}
+          onClick={() => setIsDrawerOpen(!isDrawerOpen)}
         >
           Menu
         </Button>
+        <SwipeableDrawer
+          anchor={"right"}
+          open={isDrawerOpen}
+          onOpen={() => setIsDrawerOpen(true)}
+          onClose={() => setIsDrawerOpen(false)}
+        >
+          <Container>
+            <Typography variant={"body1"}>
+              {locale === Locale.BR
+                ? STRINGS.LANGUAGE_SWITCHER.pt
+                : STRINGS.LANGUAGE_SWITCHER.en}
+            </Typography>
+
+            <Button variant={"contained"} onClick={handleLocaleClick}>
+              {locale}
+            </Button>
+          </Container>
+        </SwipeableDrawer>
       </Header>
       <TaskManagement />
-    </AppSettingsProvider>
+    </>
   );
 }
 
