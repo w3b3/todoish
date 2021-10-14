9826f9ba182cd67bfa07151fcd3a7dc6751223cb
Daniel Santos <iam@w-b.dev>
2021-10-14 09:32:28 -0300

    update menu drawer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,18 +7,42 @@ import { Logo } from "./Logo";
 import {
   Button,
   Container,
+  createStyles,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
   SwipeableDrawer,
   Typography,
 } from "@material-ui/core";
 import { Locale } from "../types/types";
 import { STRINGS } from "../strings/strings";
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    containerRoot: {
+      display: "flex",
+      flexDirection: "column",
+      height: "inherit",
+    },
+    drawerRoot: {
+      maxWidth: "90vw",
+      backgroundColor: "#ffdafb",
+    },
+    dialogRoot: {
+      flex: 1,
+    },
+  })
+);
+
 function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const { locale, setLocale } = useContext(AppSettingsContext);
+  const styles = useStyles();
   const handleLocaleClick = () => {
     setLocale(locale === "pt-br" ? "en-us" : "pt-br");
   };
+  const handleToggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
   return (
     <>
       <Header>
@@ -28,26 +52,41 @@ function App() {
           variant={"text"}
           title="Menu"
           startIcon={<i className="fas fa-bars" />}
-          onClick={() => setIsDrawerOpen(!isDrawerOpen)}
+          onClick={handleToggleDrawer}
         >
           Menu
         </Button>
         <SwipeableDrawer
+          classes={{ paper: styles.drawerRoot }}
           anchor={"right"}
           open={isDrawerOpen}
           onOpen={() => setIsDrawerOpen(true)}
           onClose={() => setIsDrawerOpen(false)}
         >
           <Container>
-            <Typography variant={"body1"}>
-              {locale === Locale.BR
-                ? STRINGS.LANGUAGE_SWITCHER.pt
-                : STRINGS.LANGUAGE_SWITCHER.en}
-            </Typography>
-
-            <Button variant={"contained"} onClick={handleLocaleClick}>
-              {locale}
-            </Button>
+            <DialogTitle>Settings menu</DialogTitle>
+            <DialogContent classes={{ root: styles.dialogRoot }}>
+              <section>
+                <Button
+                  variant={"contained"}
+                  color={"secondary"}
+                  onClick={handleLocaleClick}
+                >
+                  {locale === Locale.BR
+                    ? STRINGS.LANGUAGE_SWITCHER.pt
+                    : STRINGS.LANGUAGE_SWITCHER.en}
+                </Button>
+              </section>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                variant={"contained"}
+                color={"primary"}
+                onClick={handleToggleDrawer}
+              >
+                Close
+              </Button>
+            </DialogActions>
           </Container>
         </SwipeableDrawer>
       </Header>
