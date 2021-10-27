3021f85ebc522b3d7257156a2d671fa2b21eb1e9
Daniel Santos <iam@w-b.dev>
2021-10-27 09:53:07 -0300

    update grouping from 20 to 100

--- src/api/getAllEntries.tsx
+++ src/api/getAllEntries.tsx
@@ -23,7 +23,7 @@ export const getAllEntries = async (): Promise<GetAllEntriesResponse> => {
       };
     }
     const response = await fetch(
-      `https://${process.env.REACT_APP_ASTRA_DB_ID}-${process.env.REACT_APP_ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${process.env.REACT_APP_ASTRA_DB_KEYSPACE}/collections/${CONSTANTS.TASKS_COLLECTION}?page-size=20&profile=true`,
+      `https://${process.env.REACT_APP_ASTRA_DB_ID}-${process.env.REACT_APP_ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${process.env.REACT_APP_ASTRA_DB_KEYSPACE}/collections/${CONSTANTS.TASKS_COLLECTION}?page-size=100&profile=true`,
       {
         method: "GET",
         headers,
