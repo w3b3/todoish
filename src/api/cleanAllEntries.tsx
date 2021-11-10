b7d2892275ae5fa61af7709d24f2750329cfabd1
Daniel Santos <iam@w-b.dev>
2021-11-10 10:43:12 -0300

    handle error response

--- src/api/cleanAllEntries.tsx
+++ src/api/cleanAllEntries.tsx
@@ -3,7 +3,7 @@ import { deleteEntry } from "./deleteEntry";
 
 const cleanAllEntries = async () => {
   const res = await getAllEntries();
-  res.tasks.forEach((e: any) => deleteEntry(e.id));
+  if (res.tasks) res.tasks.forEach((e: any) => deleteEntry(e.id));
 };
 
 export { cleanAllEntries };
