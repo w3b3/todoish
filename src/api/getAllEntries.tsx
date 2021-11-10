f67336ebf73f9a0ee73a84410e0194b5cd5b5870
Daniel Santos <iam@w-b.dev>
2021-11-10 10:57:50 -0300

    update API call to use AWS Lambda

--- src/api/getAllEntries.tsx
+++ src/api/getAllEntries.tsx
@@ -1,50 +1,34 @@
-import { CONSTANTS, headers } from "./_";
-// import { mockApiResponse } from "../mocks";
-/*This conditional is used for debugging purposes only*/
-// const isLocalDevelopment = process.env.REACT_APP_ENV === "local";
-const isDevelopment = process.env.REACT_APP_ENV === "development";
+interface MyMainObjectModel {
+  creationTime: number;
+  id: string;
+  isDone: boolean;
+  lastUpdateTime: number;
+  name: string;
+  tags: string[];
+}
+interface LambdaResponse extends GetAllEntriesResponse {
+  stage: string;
+}
 
 interface GetAllEntriesResponse {
-  tasks: any;
-  rowCount: any;
-  pagination: any;
+  tasks: MyMainObjectModel[];
+  rowCount: number;
+  pagination: string;
 }
+const isDevelopment = process.env.REACT_APP_ENV === "development";
 
 export const getAllEntries = async (): Promise<GetAllEntriesResponse> => {
   try {
-    /*This conditional is used for debugging purposes only*/
-    /*if (isLocalDevelopment) {
-      console.info(process.env.REACT_APP_ENV, isDevelopment);
-      //early return interrupts the flow below
-      return {
-        tasks: mockApiResponse,
-        rowCount: 2,
-        pagination: "jsalkdj239102931",
-      };
-    }*/
     const response = await fetch(
-      `https://${process.env.REACT_APP_ASTRA_DB_ID}-${
-        process.env.REACT_APP_ASTRA_DB_REGION
-      }.apps.astra${
-        isDevelopment ? "-dev" : ""
-      }.datastax.com/api/rest/v2/namespaces/${
-        process.env.REACT_APP_ASTRA_DB_KEYSPACE
-      }/collections/${CONSTANTS.TASKS_COLLECTION}?page-size=20&profile=true`,
-      {
-        method: "GET",
-        headers,
-      }
+      `https://ohtugokvbh.execute-api.us-east-1.amazonaws.com/${
+        isDevelopment ? "d" : "p"
+      }/hello`
     );
-    const parsedResponse = await response.json();
-    const tasks = Object.entries(parsedResponse.data).map((e: any) => {
-      return {
-        ...e[1],
-      };
-    });
+    const parsedResponse: LambdaResponse = await response.json();
     return {
-      tasks: tasks,
-      rowCount: parsedResponse.profile.nested[0].queries[0].rowCount,
-      pagination: parsedResponse.pageState,
+      tasks: parsedResponse.tasks ?? [],
+      rowCount: parsedResponse.tasks.length,
+      pagination: parsedResponse.pagination,
     };
   } catch (e: any) {
     console.info("Error: getAllEntries", { e });
