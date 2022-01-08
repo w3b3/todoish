eac1864b530b74b0960434dd78db7a17c2d0fc2f
Daniel Santos <iam@w-b.dev>
2022-01-08 01:24:20 -0400

    update host and error handling logic

--- src/api/getAllEntries.tsx
+++ src/api/getAllEntries.tsx
@@ -14,27 +14,45 @@ interface GetAllEntriesResponse {
   tasks: MyMainObjectModel[];
   rowCount: number;
   pagination: string;
+  error: null | string;
 }
 export const isDevelopment = process.env.REACT_APP_ENV === "development";
 
 export const getAllEntries = async (): Promise<GetAllEntriesResponse> => {
   try {
-    /* ENV "local" relies on package.json "proxy": "https://api.brasileiro.ca",
+    /* ENV "local" relies on package.json "proxy": "https://api-dev.brasileiro.ca",
      *  In this case the up to be hit would be "/todoish/tasks" and the domain is the above */
 
     const response = await fetch(
-      isDevelopment
-        ? "https://api-dev.brasileiro.ca/todoish/tasks"
-        : "https://api.brasileiro.ca/todoish/tasks"
+      `${isDevelopment ? "" : process.env.REACT_APP_API_HOST}/todoish/tasks`,
+      {
+        method: "GET",
+        headers: {
+          "x-todoish": new Date(Date.now()).toUTCString(),
+        },
+        keepalive: true,
+        mode: "cors",
+        redirect: "error",
+        referrerPolicy: "same-origin",
+        credentials: "same-origin",
+        cache: "no-cache",
+        // body: "",
+      }
     );
     const parsedResponse: LambdaResponse = await response.json();
     return {
       tasks: parsedResponse.tasks ?? [],
       rowCount: parsedResponse.tasks.length,
       pagination: parsedResponse.pagination,
+      error: null,
     };
   } catch (e: any) {
     console.info("Error: getAllEntries", { e });
-    return e;
+    return {
+      tasks: [],
+      rowCount: 0,
+      pagination: "",
+      error: e,
+    };
   }
 };
