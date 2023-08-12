fcf34a52176795a6ff1022d2096ed9c257d604bd
Daniel S <dsantos@mileiq.com>
2023-08-11 22:59:27 -0300

    remove boilerplate

--- old/src/api/cleanAllEntries.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllEntries } from "./getAllEntries";
-import { deleteEntry } from "./deleteEntry";
-
-const cleanAllEntries = async () => {
-  const res = await getAllEntries();
-  if (res.tasks) res.tasks.forEach((e: any) => deleteEntry(e.id));
-};
-
-export { cleanAllEntries };
